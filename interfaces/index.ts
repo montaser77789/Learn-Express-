d5f0535faec639e4e34d581fe0df9288e067470b
montaser <montasergohar@gmail.com>
2025-06-10 01:58:27 +0300

    Styling product card and single product using tailwindcss

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,5 +1,6 @@
 export interface Product {
   id: number;
+  imageURL?: string;
   title: string;
   price: number;
   description: string;
--- utils/fakeData.ts
+++ utils/fakeData.ts
@@ -5,6 +5,7 @@ export const generateFakeProducts = () : Product[] => {
   return Array.from({ length: 25 }, (_, index) => {
     return {
       id: index + 1,
+      imageURL : faker.image.url() ,
       title: faker.book.title(),
       price: +faker.commerce.price({ min: 100, max: 200, dec: 0 }),
       description: faker.commerce.productDescription(), // 'Featuring Phosphorus-enhanced technology, our Fish offers unparalleled Modern performance'
